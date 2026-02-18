a5c42726e77d62df78bff53598fc6aa50a6db35f
santinodezzotti <dezzottisantino@gmail.com>
2026-02-17 22:47:47 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -23,7 +23,7 @@ def obtener_noticias():
         "Clarín - Política": "https://www.clarin.com/rss/politica/",
         "La Nación - Política": "https://www.lanacion.com.ar/arc/outboundfeeds/rss/category/politica/",
         "Infobae": "https://www.infobae.com/arc/outboundfeeds/rss/",
-        "Reuters - Politics": "https://feeds.reuters.com/Reuters/PoliticsNews",
+        "Reuters - Politics": "https://www.reutersagency.com/feed/?best-topics=politics&post_type=best",
         "BBC - Mundo": "http://feeds.bbci.co.uk/mundo/rss.xml",
         "Washington Post - Politics": "http://feeds.washingtonpost.com/rss/politics"
     }
