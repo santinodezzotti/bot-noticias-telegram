a6e47cca715372b5eff6b268ba321a516fa08c33
santinodezzotti <dezzottisantino@gmail.com>
2026-02-17 22:38:20 -0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -16,32 +16,30 @@ def enviar_mensaje(texto):
     print("STATUS:", r.status_code)
     print("RESPUESTA:", r.text)
 
-
 def obtener_noticias():
     feeds = {
         "Clarín - Política": "https://www.clarin.com/rss/politica/",
         "La Nación - Política": "https://www.lanacion.com.ar/arc/outboundfeeds/rss/category/politica/",
         "Infobae": "https://www.infobae.com/arc/outboundfeeds/rss/",
         "La Política Online": "https://www.lapoliticaonline.com/feed/rss/",
-        "BBC - Mundo": "http://feeds.bbci.co.uk/mundo/rss.xml",
         "Associated Press - Politics": "https://apnews.com/hub/politics?outputType=xml",
+        "BBC - Mundo": "http://feeds.bbci.co.uk/mundo/rss.xml",
         "Washington Post - Politics": "http://feeds.washingtonpost.com/rss/politics"
     }
 
     feedparser.USER_AGENT = "Mozilla/5.0"
     noticias_enviadas = set()
 
-   for nombre, url in feeds.items():
-    feed = feedparser.parse(url)
+    for nombre, url in feeds.items():
+        feed = feedparser.parse(url)
 
-    print(nombre)
-    print("Status:", getattr(feed, "status", "No status"))
-    print("Entries:", len(feed.entries))
-    print("Bozo:", feed.bozo)
-    print("-----")
+        print(nombre)
+        print("Entries:", len(feed.entries))
+        print("Bozo:", feed.bozo)
+        print("-----")
 
-    if not feed.entries:
-        continue
+        if not feed.entries:
+            continue
 
         mensaje = f"<b>{nombre}</b>\n\n"
         contador = 0
@@ -53,15 +51,7 @@ def obtener_noticias():
             noticias_enviadas.add(entry.link)
 
             mensaje += f"• {entry.title}\n"
-            mensaje += f"{entry.link}\n\n"
-
-            contador += 1
-            if contador == 5:
-                break
-
-        if contador > 0:
-            enviar_mensaje(mensaje)
-
+            mensaj
 
 def main():
      obtener_noticias()
